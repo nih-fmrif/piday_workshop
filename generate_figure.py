7cba761c0166475f820e5a796907de600f20e393
john lee <leej3@users.noreply.github.com>
2017-05-01 11:55:53 -0400

    Update generate_figure.py

--- generate_figure.py
+++ generate_figure.py
@@ -3,6 +3,8 @@ import matplotlib as mpl
 mpl.use('Agg')
 import seaborn as sns
 
+
+# add a comment
 df_tips = sns.load_dataset('tips')
 seaborn_plot = sns.pairplot(df_tips)
 seaborn_plot.savefig('pairplot.png')
