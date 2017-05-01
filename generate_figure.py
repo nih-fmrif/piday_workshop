451bbd8834441c9fdf5d44d69327768c51841476
john lee <leej3@users.noreply.github.com>
2017-05-01 11:49:59 -0400

    change dataframe name
    here is an extended description

--- generate_figure.py
+++ generate_figure.py
@@ -3,6 +3,6 @@ import matplotlib as mpl
 mpl.use('Agg')
 import seaborn as sns
 
-df = sns.load_dataset('tips')
-seaborn_plot = sns.pairplot(df)
+df_tips = sns.load_dataset('tips')
+seaborn_plot = sns.pairplot(df_tips)
 seaborn_plot.savefig('pairplot.png')
